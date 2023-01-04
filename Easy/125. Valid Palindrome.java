3f572fe80cb1540172025a791a57036da868ea1a
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-01-04 23:20:35 +0600

    Update 125. Valid Palindrome.java

--- Easy/125. Valid Palindrome.java
+++ Easy/125. Valid Palindrome.java
@@ -29,3 +29,33 @@ public boolean isPalindrome(String s) {
 
         return true;
     }
+
+
+//Runtime: 3 ms & Memory: 42.1 MB
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+        	cHead = s.charAt(head);
+        	cTail = s.charAt(tail);
+        	if (!Character.isLetterOrDigit(cHead)) {
+        		head++;
+        	} else if(!Character.isLetterOrDigit(cTail)) {
+        		tail--;
+        	} else {
+        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        			return false;
+        		}
+        		head++;
+        		tail--;
+        	}
+        }
+        
+        return true;
+    }
+}
