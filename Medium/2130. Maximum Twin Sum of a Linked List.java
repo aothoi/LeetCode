1938ff4aa951bd01e2b05ddecddcd7157bfeb5e8
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-07-27 21:15:58 +0600

    Create 2130. Maximum Twin Sum of a Linked List.java

---
+++ Medium/2130. Maximum Twin Sum of a Linked List.java
@@ -0,0 +1,55 @@
+/**
+Example 1:
+Input: head = [5,4,2,1]
+Output: 6
+Explanation:
+Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+There are no other nodes with twins in the linked list.
+Thus, the maximum twin sum of the linked list is 6. 
+
+Example 2:
+Input: head = [4,2,2,3]
+Output: 7
+Explanation:
+The nodes with twins present in this linked list are:
+- Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+- Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+Thus, the maximum twin sum of the linked list is max(7, 4) = 7. 
+
+Example 3:
+Input: head = [1,100000]
+Output: 100001
+Explanation:
+There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        if(head == null) return 0;
+        if(head.next == null) return head.val;
+
+        int sum = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        while(head != null){
+            nums.add(head.val);
+            head = head.next;
+        }
+    
+        for(int i=0; i<nums.size(); i++){
+            sum = Math.max(sum, nums.get(i)+nums.get(nums.size()-1-i));
+        }
+
+
+        return sum;
+    }
+}
